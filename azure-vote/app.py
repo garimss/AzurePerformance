2a87d3c5d298e008d9cb9cee4e7ac3013d1344ae
Garima Agrawal <garima.rga@gmail.com>
2021-11-25 15:14:57 +0700

    Update app.py

--- azure-vote/app.py
+++ azure-vote/app.py
@@ -164,6 +164,6 @@ if __name__ == "__main__":
     #app.run(port=9999) # local
     #app.run() # remote
     # uncomment the line below before deployment to VMSS
-    app.run(host='localhost', port=8080, threaded=True)
-    #app.run(host="0.0.0.0", threaded=True, debug=True) 
+    #app.run(host='localhost', port=8080, threaded=True)
+    app.run(host="0.0.0.0", threaded=True, debug=True) 
    
